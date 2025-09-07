211212a2678f6f601ade7138b3a1ebd8f9700d0a
Rp0115 <riju0115@gmail.com>
2025-09-07 14:38:36 -0400

    fixed loading

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,8 @@ import {
   ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { Href, Stack, router, useSegments } from "expo-router";
-import React, { useEffect } from "react"; // Removed useCallback and SplashScreen
+import { Stack, router, useSegments } from "expo-router";
+import React, { useEffect, useState } from "react"; // Removed useCallback and SplashScreen
 import "react-native-gesture-handler";
 import "react-native-reanimated";
 
@@ -23,6 +23,7 @@ function InitialLayout() {
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+  const [isNavigationReady, setNavigationReady] = useState(false);
 
   useEffect(() => {
     if (isLoading || !loaded) return;
@@ -30,14 +31,15 @@ function InitialLayout() {
     const inAuthGroup = segments[0] === "(auth)";
 
     if (user && inAuthGroup) {
-      router.replace("/(tabs)" as Href);
+      router.replace("/(tabs)");
     } else if (!user && !inAuthGroup) {
-      router.replace("/(auth)" as Href);
+      router.replace("/(auth)");
     }
+    setNavigationReady(true);
   }, [user, segments, isLoading, loaded]);
 
   // 2. Render your LoadingScreen component instead of null
-  if (!loaded || isLoading) {
+  if (!loaded || isLoading || !isNavigationReady) {
     return <LoadingScreen />;
   }
 
